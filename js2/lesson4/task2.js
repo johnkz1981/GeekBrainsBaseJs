e26e9bdc82e4bc12c1a23f8040b2cf61c6b82f34
johnkz <johnkz@inbox.ru>
2018-09-25 09:31:20 +0300

    2-4-2 finish

--- js2/lesson4/task2.js
+++ js2/lesson4/task2.js
@@ -1,5 +1,16 @@
 'use strict';
 
 const cities = document.getElementById('cities');
+const citiesArr = [];
 
-cities.innerHTML = '<li>Lorem ipsum.</li>';
+fetch('russiaLocality.json')
+    .then(result => {
+      return result.json();
+    })
+    .then(data => {
+      for (const city of data) {
+        citiesArr.push(`<option value="${city.name}">${city.name}</option>`);
+      }
+      cities.innerHTML = citiesArr.sort();
+    })
+    .catch(error => console.log(error));
